9cb43466b5869c42327d5fd31660be38f7e988b1
unknown <pavithran.rajasekaran@gmail.com>
2018-12-31 09:39:34 +0530

    Counts modified

--- TimeTracker/Controllers/UserController.cs
+++ TimeTracker/Controllers/UserController.cs
@@ -26,37 +26,21 @@ namespace TimeTracker.Controllers
         [HttpGet]
         public ActionResult Dashboard()
         {
-            var timesheetResult = _ITimeSheet.GetTimeSheetsCountByUserID(Convert.ToString(Session["UserID"]));
+            //var timesheetResult = _ITimeSheet.GetTimeSheetsCountByUserID(Convert.ToString(Session["UserID"]));
 
-            if (timesheetResult != null)
-            {
-                ViewBag.SubmittedTimesheetCount = timesheetResult.SubmittedCount;
-                ViewBag.ApprovedTimesheetCount = timesheetResult.ApprovedCount;
-                ViewBag.RejectedTimesheetCount = timesheetResult.RejectedCount;
-            }
-            else
-            {
-                ViewBag.SubmittedTimesheetCount = 0;
-                ViewBag.ApprovedTimesheetCount = 0;
-                ViewBag.RejectedTimesheetCount = 0;
-            }
-
-
-           // var expenseResult = _IExpense.GetExpenseAuditCountByUserID(Convert.ToString(Session["UserID"]));
-
-            //if (expenseResult != null)
+            //if (timesheetResult != null)
             //{
-            //    ViewBag.SubmittedExpenseCount = expenseResult.SubmittedCount;
-            //    ViewBag.ApprovedExpenseCount = expenseResult.ApprovedCount;
-            //    ViewBag.RejectedExpenseCount = expenseResult.RejectedCount;
+            //    ViewBag.SubmittedTimesheetCount = timesheetResult.SubmittedCount;
+            //    ViewBag.ApprovedTimesheetCount = timesheetResult.ApprovedCount;
+            //    ViewBag.RejectedTimesheetCount = timesheetResult.RejectedCount;
             //}
             //else
             //{
-            //    ViewBag.SubmittedExpenseCount = 0;
-            //    ViewBag.ApprovedExpenseCount = 0;
-            //    ViewBag.RejectedExpenseCount = 0;
+            //    ViewBag.SubmittedTimesheetCount = 0;
+            //    ViewBag.ApprovedTimesheetCount = 0;
+            //    ViewBag.RejectedTimesheetCount = 0;
             //}
-
+            
             return View();
         }
 
